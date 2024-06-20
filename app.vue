96816c434bb84f3875e00fa7113cfc41fa842b48
Peter <peter.volosenco@gmail.com>
2024-06-20 13:14:25 +0100

    init

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useRuntimeConfig } from '#app'
+
+const config = useRuntimeConfig()
+const apiBaseUrl = config.public.apiBaseUrl
+</script>
+
+<template>
+  <div>
+    <div>API Base URL: {{ apiBaseUrl }}</div>
+  </div>
+</template>
---
+++ components/User.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Component: User
+  </div>
+</template>
+
+<style scoped></style>
